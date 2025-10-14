13d3944c6b2c4f43a025b9be03e4433654d84616
Cassio Rosa <cassioasrosa@gmail.com>
2025-10-14 16:08:24 -0300

    add front end rota adicionar filme

--- front-end/app.py
+++ front-end/app.py
@@ -31,3 +31,18 @@ if menu == "Catalogo":
             st.table(tabela_filmes)
     else:
         st.error("Erro ao acessar a API")
+
+elif menu == "Adicionar Filme":
+    st.subheader("Adicione um Filme no catalogo")
+    titulo = st.text_input("Título do Filme")
+    genero = st.text_input("Gênero")
+    ano = st.number_input("Ano de Lançamento", min_value=1880, max_value=2100, step=1)
+    avaliacao = st.number_input("Avaliação de (0 a 10)", min_value=0.0, max_value=10.0, step=0.1)
+    if st.button("Salvar Filme"):
+        dados = {"titulo": titulo, "genero": genero, "ano": ano, "avaliacao": avaliacao}
+        response = requests.post(f"{API_URL}/filmes", params=dados)
+        if response.status_code == 200:
+            st.success("Filme adicionado com sucesso!")
+        else:
+            st.error("Erro ao adicionar o filme")
+    
